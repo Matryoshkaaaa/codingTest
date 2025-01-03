d6a11d8b9580f472e150c6810270240e474e7e55
Matryoshkaaaa <154948725+Matryoshkaaaa@users.noreply.github.com>
2025-01-03 21:00:31 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    
+    num = list(map(int," ".join(str(n)).split()))
+    print(num)
+    # print(sum(num))
+    answer = sum(num)
+    
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+    
+
+    return answer
